350ace866aaa2a4595026991c551a19cfc7fd978
Mario Blanco <mario10_bd@hotmail.com>
2023-07-25 16:25:25 +0200

    Delete items bin Fixes #10

--- src/app/create/componentGrid.tsx
+++ src/app/create/componentGrid.tsx
@@ -4,7 +4,8 @@ import { useState } from "react";
 import RGL, { WidthProvider } from "react-grid-layout";
 import "/node_modules/react-grid-layout/css/styles.css";
 import "/node_modules/react-resizable/css/styles.css";
-import { Paper } from "@mui/material";
+import { IconButton, Paper } from "@mui/material";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
 
 const ReactGridLayout = WidthProvider(RGL);
 export default function ComponentGrid(props: {
@@ -18,6 +19,11 @@ export default function ComponentGrid(props: {
     layoutItem.minW = props.movingItem?.maxX;
     layoutItem.minH = props.movingItem?.maxY;
     setlayout(layout);
+  };
+
+  const onDeleteHandler = (i: string) => {
+    delete props.items[i];
+    setlayout(layout.filter((item) => item.i !== i));
   };
   return (
     <Paper>
@@ -55,6 +61,14 @@ export default function ComponentGrid(props: {
               data-grid={item}
               onClick={() => props.setSelectedItem(props.items[item.i])}
             >
+              <IconButton
+                aria-label="delete"
+                className={styles.deleteIcon}
+                onClick={() => onDeleteHandler(item.i)}
+                size="small"
+              >
+                <DeleteForeverIcon />
+              </IconButton>
               <BasketComponent component={props.items[item.i]} />
             </div>
           );
